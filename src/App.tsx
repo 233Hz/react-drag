5d435cdf897308f848b3a15eca71e4bfed9d1bd1
BUNGA <3010086711@qq.com>
2023-10-27 15:31:44 +0800

    feat: 基本布局和拖拽功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { DragEvent, MouseEvent, useState, useEffect } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Widget {
+  type: string;
+  w: number;
+  h: number;
 }
 
-export default App
+interface WidgetBox extends Widget {
+  id: number;
+  x: number;
+  y: number;
+}
+
+let index = 1;
+let widgetX = 0;
+let widgetY = 0;
+let widget: Widget | undefined = void 0;
+const widgetList: Widget[] = [
+  {
+    type: 'pie',
+    w: 200,
+    h: 200,
+  },
+  {
+    type: 'line',
+    w: 400,
+    h: 200,
+  },
+  {
+    type: 'bar',
+    w: 400,
+    h: 200,
+  },
+];
+
+function App() {
+  const [list, setList] = useState<WidgetBox[]>([]);
+  const onDrop = (e: DragEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    console.log(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+
+    const _widget = {
+      id: index++,
+      x: e.nativeEvent.offsetX - widgetX,
+      y: e.nativeEvent.offsetY - widgetY,
+      ...widget!,
+    };
+    const arr = [...list];
+    arr.push(_widget);
+    setList(arr);
+  };
+  const onMouseDown = (e: MouseEvent<HTMLDivElement>, _widget: Widget) => {
+    widgetX = e.nativeEvent.offsetX;
+    widgetY = e.nativeEvent.offsetY;
+    widget = _widget;
+    console.log(widgetX, widgetY);
+  };
+  useEffect(() => {
+    // console.log(list);
+  }, [list]);
+  return (
+    <>
+      <div id="app" className="app">
+        <div className="widget-list">
+          {widgetList.map(item => (
+            <div
+              key={item.type}
+              className="widget"
+              draggable="true"
+              onMouseDown={e => {
+                onMouseDown(e, item);
+              }}>
+              {item.type}
+            </div>
+          ))}
+        </div>
+        <div className="panel" onDragOver={e => e.preventDefault()} onDrop={onDrop}>
+          {list.map(item => (
+            <div
+              className="box"
+              key={item.id}
+              style={{ transform: `translate(${item.x}px, ${item.y}px)`, width: `${item.w}px`, height: `${item.h}px` }}>
+              {item.type}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
